651a8d807a9f93d61fd9be778c9da08d1589875d
kartik singh prajapati <kartiksinghprajapati@gmail.com>
2022-10-13 13:49:58 +0530

    Signed-off-by: kartik singh prajapati <kartiksinghprajapati@gmail.com>

---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css'
+
+const Header=()=>{
+
+    return(
+        <div className="header" style={{borderRadius:"6px"}}>
+            <h1 className="test">Vehicle-Parking Management System</h1>
+        </div>
+    );
+}
+
+export default Header;
